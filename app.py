10c040f06da4b13ba1cc76459d321e0dcd3f0327
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-10 22:26:56 -0500

    second commit, general

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def index():
 @app.route("/getworldcup")
 def process_form():
     participants = get_teams_in_wc()
-    print(participants)
     res = get_info_on_year()
+    print(res)
     news = requests.get("https://newsapi.org/v2/everything?sources=espn,bbc-sport&q=fifa+world+cup+"+res[1]+"&apiKey=36071cd47bf64942aeb3ae57d16f664c").json()
-    return render_template("list.html.j2", data=res[0], news=news, year=res[1], participants=participants)
+    return render_template("list.html.j2", data=res[0], news=news, year=res[1], participants=participants, wc_instace = res[0][0][1])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- wcdata/worldcups.py
+++ wcdata/worldcups.py
@@ -5,16 +5,16 @@ conn = sqlite3.connect("worldcups.db")
 
 cur = conn.cursor()
 
-cur.execute('''CREATE TABLE worldcups (year text, country text,  winner text,  runnerup text,  third text,  fourth text,
+cur.execute('''CREATE TABLE worldcups (year text, instance int, country text,  winner text,  runnerup text,  third text,  fourth text,
 goalsscored int,  qualifiedteams int,  matchesplayed int,  attendance real)''')
 
 with open("WorldCups.csv") as file:
     for line in file:
         line = line.split(",")
         line[9] = line[9].replace("\n","")
-        cur.execute('''INSERT INTO worldcups VALUES({},{},{},{},{},{},{},{},{},{})'''.format('"'+line[0]+'"','"'+line[1]+'"',
+        cur.execute('''INSERT INTO worldcups VALUES({},{},{},{},{},{},{},{},{},{},{})'''.format('"'+line[0]+'"','"'+line[1]+'"',
         '"'+line[2]+'"','"'+line[3]+'"','"'+line[4]+'"','"'+line[5]+'"','"'+line[6]+'"','"'+line[7]+'"',
-        '"'+line[8]+'"','"'+line[9]+'"'))
+        '"'+line[8]+'"','"'+line[9]+'"','"'+line[10]+'"'))
 
 conn.commit()
 
