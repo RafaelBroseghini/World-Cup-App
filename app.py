ac7b13796e2dde337d20b30e761e415111e5b549
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-12 21:00:00 -0500

    roster with stats

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def index():
 def process_form():
     participants = get_teams_in_wc()
     res = get_info_on_year()
+    print(res)
     news = requests.get("https://newsapi.org/v2/everything?sources=espn,bbc-sport&q=fifa+world+cup+"+res[1]+"&apiKey=36071cd47bf64942aeb3ae57d16f664c").json()
     return render_template("list.html", data=res[0], news=news, year=res[1], participants=participants, wc_instace = res[0][0][1])
 
@@ -25,7 +26,8 @@ def process_roster():
     players = get_roster_from_country()
     matches = get_matches_in_year()
     roster = build_roster(matches, players)
-    return render_template("roster.html", roster=roster)
+    stats = build_stats(roster)
+    return render_template("roster.html", roster=stats)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- worldcup.py
+++ worldcup.py
@@ -44,7 +44,7 @@ def get_roster_from_country():
     country = request.args.get("team")
     conn = sqlite3.connect("wcdata/worldcupplayers.db")
     cur = conn.cursor()
-    cur.execute('''select matchid, playername, event from worldcupplayers where teaminit = {}'''.format("'"+country+"'"))
+    cur.execute('''select matchid, playername, event, shirtnumber from worldcupplayers where teaminit = {}'''.format("'"+country+"'"))
     players = cur.fetchall()
     return players
 
@@ -66,11 +66,26 @@ def build_roster(lst1, lst2):
         for m in matches_dict:
             if p[0] == m and p[1] not in players_dict:
                 if p[2] != "":
-                    players_dict[p[1]] = {p[2]:1}
+                    players_dict[p[1]] = {p[2]:1,"Number":p[3]}
                 else:
-                    players_dict[p[1]] = {}
-            elif p[1] in players_dict and p[2] != "":
+                    players_dict[p[1]] = {"Number":p[3]}
+            elif p[1] in players_dict and p[2] != "" and p[0] == m:
                 players_dict[p[1]][p[2]] = 1 
     return players_dict
+    # return matches_dict
 
+def build_stats(roster):
+    stats_dict = {}
+    for player in roster:
+        stats_dict[player] = {"Number":roster[player]["Number"], "Goals": 0, "Yellow Cards": 0, "Red Cards":0}
+        for stat in roster[player]:
+            for l in stat:
+                if l == "G"or l == "P":
+                    stats_dict[player]["Goals"] += 1
+                elif l == "Y":
+                    stats_dict[player]["Yellow Cards"] += 1
+                elif l == "R":
+                    stats_dict[player]["Red Cards"] += 1
+
+    return stats_dict
     
