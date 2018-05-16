7d4aa8176423546db35f4e93e79586345abeafb7
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-15 20:37:56 -0500

    cleaned comments

--- wcdata/groups.py
+++ wcdata/groups.py
@@ -1,8 +1,6 @@
-# import sqlite3
 import records
 db = records.Database("sqlite:///./groups18.db")
 
-# db = conn.cursor()
 
 db.query("CREATE TABLE groups18 (gp text, t1 text, t2 text, t3 text, t4 text)")
 
@@ -16,6 +14,3 @@ with open("groups.csv") as file:
         db.query('''INSERT INTO groups18 VALUES({},{},{},{},{})'''.format("'"+line[0]+"'","'"+line[1]+"'",
         "'"+line[2]+"'","'"+line[3]+"'","'"+line[4]+"'"))
 
-# conn.commit()
-
-# conn.close()
